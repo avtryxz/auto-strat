6261399eb25e9c43b6fe4316441c8db3a1216006
avtryxz <avtryxz@gmail.com>
2026-06-22 11:37:17 +0200

    Update Recorder.lua

--- Sources/Recorder.lua
+++ Sources/Recorder.lua
@@ -686,14 +686,27 @@ return function(ctx)
                         original = hookmetamethod(game, "__namecall", function(self, ...)
                             local method = getnamecallmethod and getnamecallmethod() or nil
                             if method == "InvokeServer" or method == "FireServer" then
-                                if typeof(self) == "Instance" and (self.ClassName == "RemoteFunction" or self.ClassName == "RemoteEvent" or self.ClassName == "UnreliableRemoteEvent") then
-                                    local args = {...}
-                                    local results = table.pack(original(self, ...))
-                                    local handler = Globals.__tds_recorder_handler
-                                    if handler then
-                                        task.spawn(pcall, handler, self, method, args, results)
+                                local isRemote = false
+                                pcall(function()
+                                    if typeof(self) == "Instance" and (self:IsA("RemoteFunction") or self:IsA("RemoteEvent") or self:IsA("UnreliableRemoteEvent")) then
+                                        isRemote = true
                                     end
-                                    return table.unpack(results, 1, results.n)
+                                end)
+                                if isRemote then
+                                    local args = {...}
+                                    local results = table.pack(pcall(original, self, ...))
+                                    if results[1] then
+                                        local resultsData = {}
+                                        for i = 2, results.n do
+                                            resultsData[i - 1] = results[i]
+                                        end
+                                        resultsData.n = results.n - 1
+                                        local handler = Globals.__tds_recorder_handler
+                                        if handler then
+                                            task.spawn(pcall, handler, self, method, args, resultsData)
+                                        end
+                                        return table.unpack(resultsData, 1, resultsData.n)
+                                    end
                                 end
                             end
                             return original(self, ...)
